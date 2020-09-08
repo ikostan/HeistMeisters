46765d2ebda2736f1daefac2e3dda82c5a01bc17
Egor Kostan <igorkostan@gmail.com>
2020-09-07 22:37:09 -0700

    Template Character created: scene + script

---
+++ Characters/TemplateCharacter/TemplateCharacter.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+const SPEED = 10
+const Max_Speed = 100
+const FRICTION = 0.1 # time in sec to reduce max speed to zero
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
