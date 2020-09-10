4cc0e3dd56b6ffaeb280645047bdb947470c0ea3
Egor Kostan <igorkostan@gmail.com>
2020-09-10 13:06:26 -0700

    code refactoring + comments

--- NPC/PlayerDetection.gd
+++ NPC/PlayerDetection.gd
@@ -14,9 +14,9 @@ func _ready():
 
 func _process(delta):
 	if player_in_view_range() and player_in_view_distance():
-		$Light2D.color = RED
 		if not $AudioAlarm.playing:
 			$AudioAlarm.play()
+		$Light2D.color = RED
 	else:
 		$Light2D.color = WHITE
 		$AudioAlarm.playing = false
@@ -24,7 +24,7 @@ func _process(delta):
 
 func player_in_view_range():
 	var camera_direction = Vector2(1,0).rotated(global_rotation)
-	var player_direction = (Player.position - global_position).normalized()
+	var player_direction = (Player.global_position - global_position).normalized()
 	
 	if abs(player_direction.angle_to(camera_direction)) < deg2rad(VIEW_RANGE):
 		return true
@@ -32,18 +32,18 @@ func player_in_view_range():
 
 	
 func player_in_view_distance():
-	var space = get_world_2d().direct_space_state
-	var collision_obj = space.intersect_ray(global_position, 
-											Player.global_position, 
-											[self], 
-											collision_mask)
-	
-	if not collision_obj:
-		return false
+	var game_space = get_world_2d().direct_space_state
+	var collided_obj = game_space.intersect_ray(global_position, 
+												Player.global_position, 
+												[self], 
+												collision_mask)
 	
 	var distance_to_player = Player.global_position.distance_to(global_position)
 	
-	if collision_obj.collider == Player and distance_to_player < VIEW_DISTANCE:
+	if not collided_obj:
+		return false
+	
+	if collided_obj.collider == Player and distance_to_player < VIEW_DISTANCE:
 		return true
 	
 	return false
